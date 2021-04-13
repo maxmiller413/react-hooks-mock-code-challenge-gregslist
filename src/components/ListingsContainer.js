5fe104401ed9015b1eda435b85c16fe474d06a0f
Max Miller <maxmiller413@gmail.com>
2021-04-12 21:29:42 -0500

    1 del

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ListingCard from "./ListingCard";
 
 function ListingsContainer({listing, onDeleteList}) {
+
+  const [sorted, setSorted] = useState(false)
 
   const listingArr = listing.map(list => (
     <ListingCard 
       key={list.id}
       list={list}
       onDeleteList={onDeleteList}
+      
     />
   ))
 
+  const sortedArr = [...listing]
+    sortedArr.sort(function (a, b) {
+      return a.location.localeCompare(b.location)
+    })
+    
+    const mappedArr = sortedArr.map(list => (
+      <ListingCard 
+        key={list.id}
+        list={list}
+        onDeleteList={onDeleteList}
+      />
+    ))
+
+  function handleSort() {
+    setSorted(sorted => !sorted)
+  }
+
   return (
     <main>
+      <button onClick={handleSort} > SORTED </button>
       <ul className="cards">
-        {listingArr}
+        {sorted ? mappedArr : listingArr}
       </ul>
     </main>
   );
