cb20bc83441cd8c31bd1b9674945c8abe8ece29c
Max Miller <maxmiller413@gmail.com>
2021-04-12 20:49:26 -0500

    core del

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+const [listing, setListing] = useState([])
+const [searchForm, setSearchForm] = useState("")
+
+useEffect(() => {
+  fetch("http://localhost:6001/listings")
+    .then(r => r.json())
+    .then(data => setListing(data))
+}, [])
+
+function handleDeleteList(deletedListing){
+  const updatedListing = listing.filter(listing => (
+    listing.id !== deletedListing.id
+  ))
+  setListing(updatedListing)
+}
+
+const displayedListings = listing.filter(listing => (
+  listing.description.toLowerCase().includes(searchForm.toLowerCase())
+))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearchForm} />
+      <ListingsContainer listing={displayedListings} onDeleteList={handleDeleteList} />
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({list, onDeleteList}) {
+
+  const [fav, setFav] = useState(false)
+
+  const {id, description, image, location} = list
+
+  function handleFavClick() {
+    setFav(fav => !fav)
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then(r => r.json())
+      .then(() => onDeleteList(list))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {fav ? (
+          <button onClick={handleFavClick} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={handleFavClick} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button onClick={handleDeleteClick} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
